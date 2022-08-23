2c4141aed118abeb80761a16cb683093e66b03c8
Bryan Goodrich <Bryan.Goodrich@smud.org>
2022-08-22 17:43:55 -0700

    Update roshambo bot

--- freecodecamp/roshambo/RPS.py
+++ freecodecamp/roshambo/RPS.py
@@ -1,45 +1,125 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 import random
 import numpy as np
 
-# Tracking 2 history states
-observations = ("RR", "RP", "RS", "PR", "PP", "PS", "SR", "SP", "SS")
-choices = ("R", "P", "S")
-size = (len(observations), len(choices))
-q = np.zeros(size)
-last_play = ""
+# include prediction in state
+# store state and transition incrementally, adding history incrementally?
+# store list of rewards to observe progression
 
-def player(prev_play, history = []):
-    global last_play
-    lr = 0.1
-    gamma = 0.99
-    explore = 1
-    decay = 0.001
-    min_explore = 0.01
-    history.append(prev_play)
+class Qtable:
+    def __init__(self, 
+        learning_rate,
+        discount_factor,
+        history_size = 3,
+        exploration=(0.7, 0.01, 0.0001)):
+        """ Description of the Class """
+        
+        self.actions = ("R", "P", "S")
+        self.size = (len(self.actions)**history_size, len(self.actions))
+        self.reward_space = (0.4, 0, -0.2)
+        
+        self.lr = learning_rate
+        self.df = discount_factor
+        self.decay_rate = exploration[1]
+        self.explore_minimum = exploration[2]
+        self.prediction = random.choice(self.actions)
+        
+        self.q = np.zeros(self.size)
+        self.explore_start = exploration[0]
+        self.explore = exploration[0]
+        self.rewards = [0]
     
-    # state needs to be -2:-1 and next_state is -2:
-    reward = get_reward(last_play, prev_play)
-    q[state, action] = (1-lr) * q[state, action] + lr*(reward + gamma*max(q[next_state,:]))
+    def __getitem__(self, key):
+        return self.actions.index(key)
     
-    if len(history) < 3:
-        return random.choice(choices)
+    @property
+    def table(self):
+        return self.q
     
-    rnd = random.random()
-    obs = "".join(history[-2:])
-    state = observations.index(obs)
+    def position(self, *keys):
+        rev = keys[::-1]
+        n = len(self.actions)
+        return sum([self[k]*n**i for i, k in enumerate(rev)])
     
-    if rnd < explore:
-        action = random.choice([0, 1, 2])
-    else:
-        action = choices[np.argmax(q[state, :])]
+    def compute_reward(self, prev):
+        previous_play = self.actions.index(prev)
+        predicted_play = self.actions.index(self.prediction)
+        offset = (previous_play - predicted_play) % len(self.actions)
+        reward = self.reward_space[offset]
+        
+        if reward > 0:
+            self.lr = max(self.lr*.9, 0.2)
+            self.df = max(self.df*.95, 0.2)
+        else:
+            self.lr = min(self.lr*1.05, 1.0)
+            self.df = min(self.df*1.02, 1.0)
+        
+        return reward
     
-    explore = max(explore-decay, min_explore)
-    return choices[action]
+    def evaluate(self, history):
+        state = self.position(*history[:-1])
+        next_state = self.position(*history[1:])
+        reward = self.compute_reward(history[-1])
+        return state, next_state, reward
+    
+    def bellman(self, alpha, gamma, reward, old_state, max_new_state):
+        beta = 1 - alpha
+        return beta*old_state + alpha*(reward + gamma*max_new_state)
+    
+    def update(self, history):
+        self.explore = max(self.explore-self.decay_rate, self.explore_minimum)
+        old, new, reward = self.evaluate(history)
+        action = self.actions.index(self.prediction)
+        self.rewards.append(reward)
+
+        old_state = self.q[old, action]
+        new_state = max(self.q[new, :])
+        
+        value = self.bellman(self.lr, self.df, reward, old_state, new_state)
+        
+        self.q[old, action] = value
+        new_prediction = self.predict(new)
+        self.prediction = new_prediction
+        return new_prediction
+    
+    def predict(self, state):
+        if random.random() < self.explore:
+            return random.choice(self.actions)
+        else:
+            best_guess = np.argmax(self.q[state, :])
+            return self.actions[best_guess]
+    
+    def reset(self):
+        self.q = np.zeros(self.q.shape)
+        self.explore = self.explore_start
+        self.rewards = [0]
+        self.prediction = "R"
+    
+    def __del__(self):
+        print(self.rewards)
+        print(np.round(self.q, 2))
+
+
+def player_generator(lr, gamma, length, verbose=False):
+    q = Qtable(lr, gamma, length)
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    
+    def inner(prev, history=[]):
+        if not prev:
+            history.clear()
+            q.reset()
+            history += random.choices(q.actions, k=length)
+        
+        if len(history) > length:
+            _ = history.pop(0)
+        
+        history.append(prev if prev else "R")
+        prediction = q.update(history)
+        if verbose:
+            print(sum(q.rewards))
+        return ideal_response[prediction]
+    return inner
+
+player = player_generator(0.85, .9, 3)
 
 
 
-
-# Bellman Equation
-# q[state, action] = (1-lr)*q[state, action] + lr*(r + g*max(q[state, :]))
-# 
